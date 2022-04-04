73f65478d3ed51963253317e6f449d2e5cc45000
Boris Guiñez <borisandres.guinez@gmail.com>
2022-04-03 22:34:51 -0400

    Varios cambios finales

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,21 +1,5 @@
 $(function(){
-    $('h3').dblclick(function(){
-        $(this).css({
-            color:'red',
-         })
-    })
-
-   $('.card-title').click(function(){
-        $('p').hide('slow')
-   })
-
-   $('#enviarcorreo').click(function(event){
-        
-    //event.preventDefault();
-    //llamar a funcion que valida
-    //console.log(event);
-    alert("El Correo fue enviado correctamente")
-    //this.submit();
-
+    $('#enviar-mensaje').click(function(event){
+    alert("El Mensaje se Envio de manera exitosa")
     })
 })
